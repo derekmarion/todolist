3a5862b6743adab1fcf8f01911c9c6193beab79d
Frank_Gary <blackrebelradio1992@gmail.com>
2023-10-11 09:19:22 -0500

    new update

--- js/example.js
+++ js/example.js
@@ -11,7 +11,18 @@ function getInput(promptText) {
 }
 
 async function runApp() {
+    // The prompt() method displays a dialog box that prompts the user for input.
+    prompt("chooose your option\n
+        1. display TODos\n 2. add new ToDo")
     let userInput = await getInput("enter task name\n");
+    switch(userInput){
+        case 1: console.log('\nToDos\n')
+         console.log(todos)
+         case 2: console.log()
+
+    }
+    
+    // return userInput
     
     // {
     //   userInput = string;
---
+++ js/switch_fucntion.js
@@ -0,0 +1,57 @@
+// function toDo(userInput){
+//     let userInput = prompt("What's your sign?");
+
+//     // if (sign.toLowerCase() === "scorpio") {
+//     //   alert("Wow! I'm a Scorpio too!");
+//     // }
+    
+//     // // there are many ways to use the prompt feature
+//     // sign = window.prompt(); // open the blank prompt window
+//     // sign = prompt(); //  open the blank prompt window
+//     // sign = window.prompt("Are you feeling lucky"); // open the window with Text "Are you feeling lucky"
+//     // sign = window.prompt("Are you feeling lucky", "sure"); // open the window with Text "Are you feeling lucky" and default value "sure"
+
+
+// //    let toDoArr = [];
+// //     console.log("  welcome to  out to do list app");
+// //     console.log("----------------------------------");
+// //     console.log("enter one of these numbers!")
+// //     console.log("1 - display ToDo's\n2 - add toDo\n3 - remove toDo\n4 - exit")
+
+// //     const displayToDo = () => {
+// //         console.log(`TO DOs\n${toDoArr}`)
+// //     }
+
+
+
+
+// //     if (userInput === 1){
+// //         return displayToDo();
+// //     } else if (userInput === 2){
+// //         return addToDo();
+// //     } else if (userInput === 3){
+// //         return romeoveToDo()
+// //     }else if (userInput === 4){
+// //         return exitApp()
+// //     }else {
+// //         return
+// //     }
+// }
+// console.log(toDo());
+
+console.log("  welcome to  out to do list app");
+console.log("----------------------------------");
+console.log("enter one of these numbers!")
+console.log("1 - display ToDo's\n2 - add toDo\n3 - remove toDo\n4 - exit")
+
+let sign = prompt("What's your sign?");
+
+if (sign.toLowerCase() === "scorpio") {
+  alert("Wow! I'm a Scorpio too!");
+}
+
+// there are many ways to use the prompt feature
+sign = window.prompt(); // open the blank prompt window
+sign = prompt(); //  open the blank prompt window
+sign = window.prompt("Are you feeling lucky"); // open the window with Text "Are you feeling lucky"
+sign = window.prompt("Are you feeling lucky", "sure"); // open the window with Text "Are you feeling lucky" and default value "sure"
